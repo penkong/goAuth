5ec712f5f9fa85523171522ac2e41b6a7e6a64f0
mazdak <nazemi.works@gmail.com>
2021-02-28 21:37:00 +0400

    through add logic for server

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/google/uuid v1.2.0
+	github.com/lib/pq v1.9.0
 	github.com/spf13/viper v1.7.1
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+	"github.com/penkong/goAuth/util"
+	// "github.com/penkong/goAuth/api"
+	// db "github.com/penkong/goAuth/db/sqlc"
+	// "github.com/penkong/goAuth/util"
+)
+
+var db *sql.DB
+
+func main() {
+	config, err := util.LoadConfig('.')
+	
+}
